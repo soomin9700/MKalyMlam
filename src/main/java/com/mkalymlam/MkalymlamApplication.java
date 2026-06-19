e6f1269578c7180bf37496df01ba7e50bdff6cfc
MiantsaSarindra <soominsoomin65@gmail.com>
2026-06-19 11:45:27 +0300

    initialisation de projet

---
+++ src/main/java/com/mkalymlam/MkalymlamApplication.java
@@ -0,0 +1,13 @@
+package com.mkalymlam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkalymlamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkalymlamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkalymlam/MkalymlamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkalymlam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkalymlamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
