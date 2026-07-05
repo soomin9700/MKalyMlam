481a6ffc55f5202e136e87c72ce9d1bbb7cfb79a
nampoinafandresena <razafindraibenampoina98@gmail.com>
2026-07-05 22:37:13 +0300

    Retirer le dossier target du suivi git

--- target/classes/data-lot.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "lotIngredient" ("idIngredient", "dateReception", "datePeremption", "quantiteInitiale", "quantiteRestante", "prixAchatUnitaire")
-VALUES 
-(1, '2026-06-25', '2026-07-10', 50.00, 35.00, 0.85),
-(2, '2026-06-20', '2026-07-15', 15.00, 8.50, 6.50),
-(5, '2026-06-28', '2026-07-20', 25.00, 20.00, 2.30);
