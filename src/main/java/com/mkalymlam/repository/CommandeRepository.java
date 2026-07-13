9726ae0381d026cc4b2a45864343cc289ee8f25c
nampoinafandresena <razafindraibenampoina98@gmail.com>
2026-07-13 23:01:39 +0300

    zay vao nety + css

--- src/main/java/com/mkalymlam/repository/CommandeRepository.java
+++ src/main/java/com/mkalymlam/repository/CommandeRepository.java
@@ -14,6 +14,7 @@ public interface CommandeRepository extends JpaRepository<Commande, Long> {
     // @Query("SELECT COALESCE(SUM(c.montantTotal), 0) FROM Commande c WHERE c.sessionTruck.idSession = :idSession")
     // Double sumMontantTotalByIdSession(@Param("idSession") Long idSession);
     @Query(value = "SELECT COALESCE(SUM(c.\"montantTotal\"), 0) FROM commande c WHERE c.\"idSession\" = :idSession", nativeQuery = true)
+    Double sumMontantTotalByIdSession(@Param("idSession") Long idSession);
 
     List<Commande> findByStatutCommande_Libelle(String libelle);
 
