493e4f48209866f08394f9e459cc023c6ae12fff
fandresenaloic4-web <fandresenaloic4@gmail.com>
2026-07-15 09:53:01 +0300

    ajout de la classe csvsrvice

---
+++ src/main/java/com/mkalymlam/service/CsvExcelImportService.java
@@ -0,0 +1,16 @@
+package com.mkalymlam.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CsvExcelImportService {
+    
+    // Méthodes pour importer CSV et Excel
+    public void importCsv(String filePath) {
+        // Logique d'import CSV
+    }
+    
+    public void importExcel(String filePath) {
+        // Logique d'import Excel
+    }
+}
